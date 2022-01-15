0d830fb4b8f2e4689968f972b29c028104b97be0
Hyunsoo, Chung <admin@hyonsu.com>
2022-01-15 19:52:08 +0900

    월드 초기화 명령어 확인 추가

--- commands/server/resetworld.js
+++ commands/server/resetworld.js
@@ -1,3 +1,5 @@
+const { MessageActionRow , MessageButton } = require('discord.js');
+
 const Server = require('../../class/server');
 
 module.exports = async interaction => {
@@ -7,9 +9,37 @@ module.exports = async interaction => {
         ephemeral: true
     });
 
-    await interaction.deferReply({
-        ephemeral: true
+    const msg = await interaction.reply({
+        fetchReply: true,
+        content: '정말로 월드를 초기화하시겠습니까? 초기화한 뒤에는 복구할 수 없습니다.',
+        ephemeral: true,
+        components: [
+            new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setCustomId('delete')
+                        .setStyle('DANGER')
+                        .setLabel('삭제')
+                )
+        ]
     });
+
+    try {
+        const i = await msg.awaitMessageComponent({
+            time: 30000
+        });
+        await i.update({
+            content: '월드 초기화 중입니다...',
+            ephemeral: true,
+            components: []
+        });
+    } catch(e) {
+        msg.components[0].components[0].setDisabled();
+        return interaction.editReply({
+            content: '시간이 초과되었습니다.',
+            components: msg.components
+        });
+    }
 
     server.resetWorld();
     return interaction.editReply('월드를 리셋하였습니다!');
